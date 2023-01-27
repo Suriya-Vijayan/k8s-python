cb259ff4abed8815c305eac70083407730e8e160
“Suriya-Vijayan” <suriyavijayank@gmail.com>
2023-01-27 10:25:19 +0000

    2nd commit

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-#templates = Jinja2Templates(directory="/code")
-templates = Jinja2Templates(directory="C:/Users/SURIYA VIJAYAN/Desktop/k8_project")
+templates = Jinja2Templates(directory="/code")
+#templates = Jinja2Templates(directory="C:/Users/SURIYA VIJAYAN/Desktop/k8_project")
 
 @app.get("/")
 def form_post(request: Request):
